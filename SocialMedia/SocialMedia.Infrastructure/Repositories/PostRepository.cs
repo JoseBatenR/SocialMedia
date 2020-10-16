1ecf6d48aaedf623d0f1329d43bd9cd480533423
joselbr <joselbr1989@gmail.com>
2020-10-15 22:55:20 -0600

    Creación de estructura básica de proyecto

---
+++ SocialMedia/SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts(){
+            var post = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"http://misapis.com/{x}",
+                UserId = 2
+            });
+
+            return post;
+        }
+    }
+}
